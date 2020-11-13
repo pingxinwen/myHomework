3eeb5ea76ed69294945be4440664d5375b253ba1
pingxinwen <zhangdewen0224@gmail.com>
2020-11-14 01:09:40 +0800

    设置了拦截器，可以将未登录的行为重定向到登录页面

---
+++ exp4/src/main/java/com/example/exp4_springbootdemo/Exp4SpringbootdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.exp4_springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exp4SpringbootdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exp4SpringbootdemoApplication.class, args);
+    }
+
+}
